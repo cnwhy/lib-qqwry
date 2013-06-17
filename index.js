f41cc98ef0829a537148bdf2ebb924bd71cbcca8
Wang HangYang <w.why@163.com>
2013-06-17 18:40:09 +0800

    大改动，查询效率大提升
    1.改变了老的Buffrer操作，IP地址的转换等算法。
    2.增加了IP段查询功能。
    3.换了一个GBK编码库文件，转换更快了。

--- index.js
+++ index.js
@@ -1,72 +1,117 @@
-var fs = require('fs'),
-	GBK = require("./GBK.js");
-var dbug = false;
+var fs = require('fs')
+	//,EventEmitter = require("events").EventEmitter
+	//,util = require("util")
+	//,iconv = require('iconv-lite') //iconv.decode(, 'GBK');
+	,GBK = require("./gbk.js");
 var IP_RECORD_LENGTH = 7,
 	REDIRECT_MODE_1 = 0x01,
 	REDIRECT_MODE_2 = 0x02,
-	ipBegin,ipEnd,
-	ipFileBuffer=null,ipFileBuffers=[],ipFmax=0,
+	//IP_REGEXP = /^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/,
+	IP_REGEXP1 = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/;
+	
+var dbug = false,
+	ipBegin,ipEnd,ipCount
+	ipFileBuffer=null,ipFmax=0,
 	unArea ="未知地区",unCountry = "未知国家";
 
 exports.DBUG = function(a){dbug=a;}
-
 exports.info = function(dataPath){
-	qqWryPath = typeof(dataPath) == "string" ? dataPath : (__dirname + "/qqwry.dat");
-	var rs = fs.createReadStream(qqWryPath);
-	var data,n=0;
+	dataPath = typeof(dataPath) == "string" ? dataPath : (__dirname + "/data/qqwry.dat");
 	var callback = typeof arguments[arguments.length-1] == "function" ? arguments[arguments.length-1] : function(){};
-	rs.on("data", function (chunk){
-		ipFileBuffers.push(chunk);
-		ipFmax += chunk.length;
-	});
-	rs.on("end", function () {
-		switch(ipFileBuffers.length) {//拼接Buffer
-			case 0: 
-				ipFileBuffer = new Buffer(0);
-				break;
-			case 1: 
-				ipFileBuffer = ipFileBuffers[0];
-				break;
-			default:
-				ipFileBuffer = new Buffer(ipFmax);
-				for (var i = 0, pos = 0, l = ipFileBuffers.length; i < l; i++) {
-					var chunk = ipFileBuffers[i];
-					chunk.copy(ipFileBuffer, pos);
-					pos += chunk.length;
-				}
-			break;
-		}
-		ipFileBuffers = null;
-		ipBegin = setBuffer4();
-		ipEnd = setBuffer4(4);
-		ipCount = (ipEnd-ipBegin)/7+1;
-		console.log("IP Server Start!(Begin:"+ipBegin+" End:"+ipEnd + " Count:" + ipCount + ")");
-		callback();
-	});
+	ipFileBuffer = fs.readFileSync(dataPath);
+	ipBegin = ipFileBuffer.readUInt32LE(0,true);
+	ipEnd = ipFileBuffer.readUInt32LE(4,true);
+	ipCount = (ipEnd-ipBegin)/7+1;
+	console.log("==== IP Server Start! ==== [Begin:"+ipBegin+" End:"+ipEnd + " Count:" + ipCount + "]");
+	callback.call(this);
+	return this;
 }
 
-exports.SearchIPLocation = function(ip,callback){
-	var ipArray = ip.split("."),ip,b=ipBegin,e=ipEnd,g,loc={"ip":ip};
-	if(!/^(\d{1,3}\.){3}\d{1,3}$/.test(ip)){ throw ("The IP address is not normal!"); };
-	xH = "0x";
-	for(var i=0;i<4;i++){
-		ipArray[i] = ipArray[i]-0;
-		if(ipArray[i]>255){throw ("The IP address is not normal!");}
-		var h = ipArray[i].toString(16)
-		xH += h.length == 1 ? "0" + h : h;
+//查询IP的地址信息
+exports.searchIPLocation = function(IP){
+	//IP = "255.255.255.255";
+	var ip = this.ipToInt(IP),
+		g = LocateIP(ip),
+		loc={};
+	if(g == -1){return {"ip":IP,"Country":unArea,"Area":unCountry};}
+	var add = setIPLocation(g);
+	loc.ip = this.intToIp(ip);
+	loc.Country = add.Country;
+	loc.Area = add.Area;
+	dbug && console.log(loc);
+	return loc;
+}
+
+//查询IP段的地址信息;
+exports.searchIPLocations = function(bginIP,endIP,callback){
+	var _bIP = this.ipToInt(bginIP)
+		,_endIP = this.ipToInt(endIP)
+		,b_g = LocateIP(_bIP)
+		,e_g = LocateIP(endIP)
+		,ips = [];
+	for(var i = b_g; i<= e_g ; i+= IP_RECORD_LENGTH){
+		var loc = {},add = setIPLocation(i);
+		loc.begIP = this.intToIp(ipFileBuffer.readUInt32LE(i,true));
+		loc.endIP = this.intToIp(ipFileBuffer.readUInt32LE(setBuffer3(i+4),true));
+		loc.Country = add.Country;
+		loc.Area = add.Area;
+		ips.push(loc);
 	}
-	ip = xH - 0;
-	var ipwz;
-	g = LocateIP(ip);//查寻ip所在ip段;
-	if(g == -1){//未找到此IP的地址记录
-		return {"ip":ip,"Country":unArea,"Area":unCountry};
+	if(typeof callback === "function"){callback(ips);}
+	return ips;
+}
+
+//ip地址转int
+exports.ipToInt = function(IP){
+	var result = IP_REGEXP1.exec(IP),ip;
+	if(result){
+		var ip_Arr = result.slice(1);
+		ip =(parseInt(ip_Arr[0]) << 24 
+			| parseInt(ip_Arr[1]) << 16
+			| parseInt(ip_Arr[2]) << 8
+			| parseInt(ip_Arr[3])) >>> 0;
+	}else if(/^\d+$/.test(IP) && (ip=parseInt(IP))>=0 && ip <= 0xFFFFFFFF ){
+		return ip;
+	}else{
+		throw ("The IP address is not normal!");
 	}
-	ipwz = setBuffer3(g+4); //2723489
-	ipwz += 4;//跳过32位的IP地址;
-	var lx = setIpFile(ipwz,1)[0];
+	return ip;
+}
+
+//int转ip地址
+exports.intToIp = function(INT){
+	if(INT < 0 || INT > 0xFFFFFFFF){
+		throw ("The number is not normal!");
+	}
+	return (INT>>>24) + "." + (INT>>>16 & 0xFF) + "." + (INT>>>8 & 0xFF) + "." + (INT>>>0 & 0xFF);
+}
+
+exports.infoEmit = function(a,b){
+	var o = this;
+	setTimeout(function(){o.info(a,b)});
+	return this;
+}
+
+exports.emitSearchIPLocations = function(a,b,c){
+	var o = this;
+	setTimeout(function(){o.searchIPLocations(a,b,c)});
+	return this;
+}
+
+exports.info_e = function(a,b){
+	var o = this;
+	setTimeout(function(){o.info(a,b);});
+	return this;
+}
+
+//查找地址信息
+function setIPLocation(g){
+	var ipwz = setBuffer3(g+4) + 4; //2723489
+	var lx = ipFileBuffer.readUInt8(ipwz)
+		,loc={};
 	if(lx == REDIRECT_MODE_1){//Country，ipwz1 需要根据给定偏移处标识再判断
-        ipwz = setBuffer3(ipwz+1); //读取国家偏移 (318461)
-		lx = setIpFile(ipwz,1)[0]; //再次获取标识字节
+        ipwz = setBuffer3(ipwz+1); //读取国家偏移
+		lx = ipFileBuffer.readUInt8(ipwz); //再次获取标识字节
 		var Gjbut;
 		if (lx == REDIRECT_MODE_2){ //再次检查标识字节
 			Gjbut = setIpFileString(setBuffer3(ipwz+1));
@@ -88,13 +133,12 @@ exports.SearchIPLocation = function(ip,callback){
 		loc.Country = GBK.dc_GBK(Gjbut);
 		loc.Area = ReadArea(ipwz);
 	}
-	dbug && console.log(loc);
 	return loc;
 }
 
 //读取Area
 function ReadArea(offset){
-	var one = setIpFile(offset,1)[0];
+	var one = ipFileBuffer.readUInt8(offset);
 	if (one == REDIRECT_MODE_1 || one == REDIRECT_MODE_2) {
 		var areaOffset = setBuffer3(offset + 1);
 		if (areaOffset == 0)
@@ -107,46 +151,15 @@ function ReadArea(offset){
 	}
 }
 
-function setBuffer4(n){
-	var i = n || 0;
-	return (new Buffer(setIpFile(i,4))).readUInt32LE(0);
-}
-
+//获取3字节的索引地址;
 function setBuffer3(n){
-	var i = n || 0;
-	var arr = setIpFile(i,3);
-	arr[3] = 0;
-	return (new Buffer(arr)).readUInt32LE(0);
-}
-
-//转化制定位数的LITTLE-ENDIAN为BIG-ENDIAN
-function LITTLEtoBIG(data,begin,end){
-	dbug && console.log(data);
-	var int_=0.0 , i=begin || 0; max = end || (data.length-1);
-	for(var n = 0;n<data.length;n++){
-		int_ |= (data[i++] << (8*n));
-		dbug && console.log(int_.toString(16));
-		if(i>max){break;}
-	}
-	return int_;
-}
-
-//(从拼接的Buffer中)读取指定字节,返回数组
-function setIpFile(Begin,Long){
-	var B = Begin || 0,L = Long || 1,toarr = [];
-	B = B < 0 ? 0: B;
-	L = L <= 0 ? 1 : B+L>ipFileBuffer.length? ipFileBuffer.length-B:L;
-	if(B<0 || B+L>ipFileBuffer.length) {return []};
-	for(var i = B; i <= B+L ;i++){
-		toarr.push(ipFileBuffer[i]);
-	}
-	return toarr;
+	return ipFileBuffer.readUInt32LE(n,true) & 0xFFFFFF;
 }
 
 //(拼接的Buffer中)读取字节,直到为0x00结束,返回数组
 function setIpFileString(Begin){
 	var B = Begin || 0,toarr = [], M = ipFileBuffer.length;
-	B = B < 0 ? 0: B;
+	B = B < 0 ? 0 : B;
 	for(var i = B ; i < M ;i++){
 		if(ipFileBuffer[i] == 0){
 			return toarr;
@@ -158,34 +171,34 @@ function setIpFileString(Begin){
 
 // 取得begin和end中间的偏移(用于2分法查询);
 function GetMiddleOffset(begin, end ,recordLength) {
-	var records = (end - begin) / recordLength;
-	records >>= 1;
-	if (records == 0) records = 1;
-	return begin + records * recordLength;
+	var records = ( (end - begin) / recordLength >> 1 ) * recordLength + begin; 
+	//return records == begin ? records + recordLength : records;
+	return records ^ begin ? records : records + recordLength;
 }
 
 //2分法查找指定的IP偏移
 function LocateIP(ip){
-	var g,endip
-	for (var b = ipBegin, e = ipEnd; b < e; ) {
+	var g,endip,temp;
+	for (var b=ipBegin,e=ipEnd;b<e;){
 		g = GetMiddleOffset(b,e,IP_RECORD_LENGTH);//获取中间位置
-		var temp = setBuffer4(g);
+		temp = ipFileBuffer.readUInt32LE(g,true);
 		if(ip>temp){
 			b = g;
 		}else if(ip<temp){
 			if(g == e){
-				g -= IP_RECORD_LENGTH
+				g -= IP_RECORD_LENGTH;
 				break;
 			}
 			e = g;
 		}else{
-			return g;
+			break;
 		}
 	}
-	var endip = setBuffer4(setBuffer3(g+4),4);	//再次验证查到的偏移处的结束IP段与ip比较；
-	if(ip<=endip){
-		return g;
-	}else{
-		return -1;
+	if(dbug){
+		endip = ipFileBuffer.readUInt32LE(setBuffer3(g+4),true); //再次验证查到的偏移处的结束IP段与ip比较；
+		if(ip>endip){
+			return -1;
+		}
 	}
+	return g;
 }
